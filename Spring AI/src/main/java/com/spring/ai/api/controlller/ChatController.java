919f7421b0953ddf050daf0a4f258b627bdece35
Shivam-GitLab <shivam_223033@saitm.org>
2026-09-09 21:48:43 +0530

    feat - chat-with-template

--- Spring AI/src/main/java/com/spring/ai/api/controlller/ChatController.java
+++ Spring AI/src/main/java/com/spring/ai/api/controlller/ChatController.java
@@ -2,7 +2,9 @@ package com.spring.ai.api.controlller;
 
 import com.spring.ai.api.request.ChatRequest;
 import com.spring.ai.api.response.ChatResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,6 +20,13 @@ public class ChatController {
     @PostMapping("/chat")
     public ChatResponse chat(@RequestBody ChatRequest request) {
 
+        /*  Your Controller handler
+        → ChatClient.prompt().user("...").call()
+            → Spring AI builds a Prompt object
+                → OpenAiChatModel sends HTTP POST to api.openai.com/v1/chat/completions
+                    → Response is parsed into ChatResponse
+                        → .content() extracts the text*/
+
         String response = chatClient
                 .prompt()
                 .user(request.message())
@@ -29,5 +38,26 @@ public class ChatController {
                 response,
                 "gpt-5.6-sol-2"
         );
+    }
+
+    // PROMPT TEMPLATE
+    // INJECT SOME VARIABLES INTO THE PROMPT
+    // AND CHANGE PROMPT DYNAMICALLY BASED ON USER INPUT
+    @GetMapping("/chat-with-template")
+    public String chatTemplate(@RequestParam String domain, @RequestParam String question) {
+        String promptTemplate = """
+                You are a world-class expert in {domain}.
+                Answer the following question clearly and concisely.
+                
+                Question: {question}
+                """;
+        return chatClient
+                .prompt()
+                .user(u -> u.text(promptTemplate)
+                        .param("domain", domain)
+                        .param("question", question)
+                )
+                .call()
+                .content();
     }
 }
