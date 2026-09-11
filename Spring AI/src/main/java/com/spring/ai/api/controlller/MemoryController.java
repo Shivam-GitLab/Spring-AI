99fdaecab5eb670323a2cb880eacf3bbac5915ca
Shivam-GitLab <shivam_223033@saitm.org>
2026-09-11 23:55:08 +0530

    ref - remove azure

---
+++ Spring AI/src/main/java/com/spring/ai/api/controlller/MemoryController.java
@@ -0,0 +1,15 @@
+package com.spring.ai.api.controlller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/ai")
+@RequiredArgsConstructor
+public class MemoryController {
+    private final ChatClient chatClient;
+    private final ChatMemory chatMemory;
+}
