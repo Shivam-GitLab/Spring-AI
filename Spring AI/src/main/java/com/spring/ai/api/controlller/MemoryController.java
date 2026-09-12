b554b1677dca5f2776b837ae9adb43d7aa613af2
Shivam-GitLab <shivam_223033@saitm.org>
2026-09-12 15:08:48 +0530

    Spring AI : Add Memory to AI | ChatMemory Interface | JDBC Postgres

--- Spring AI/src/main/java/com/spring/ai/api/controlller/MemoryController.java
+++ Spring AI/src/main/java/com/spring/ai/api/controlller/MemoryController.java
@@ -3,7 +3,9 @@ package com.spring.ai.api.controlller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,4 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemoryController {
     private final ChatClient chatClient;
     private final ChatMemory chatMemory;
+
+    @GetMapping("/chat-momory")
+    public String chatMemoryMethod(@RequestParam String message,
+                                   @RequestParam String conversationId ){
+        return chatClient
+                .prompt()
+                .user(message)
+                .advisors(
+                        advisorSpec ->
+                        advisorSpec.param(ChatMemory.CONVERSATION_ID, conversationId))
+                .call()
+                .content();
+    }
 }
--- Spring AI/src/main/java/com/spring/ai/config/AIConfig.java
+++ Spring AI/src/main/java/com/spring/ai/config/AIConfig.java
@@ -4,25 +4,28 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AIConfig {
+
     @Bean
-    public ChatMemory chatMemory(){
+    public ChatMemory chatMemoryTemp(){
         return MessageWindowChatMemory
                 .builder()
                 .maxMessages(10)  // 10 last messages at once, 11th message -> 1st removed
                 .build();
     }
-/*    @Bean
+
+    @Bean
     public ChatMemory chatMemory(JdbcChatMemoryRepository repository) {
         return MessageWindowChatMemory.builder()
                 .chatMemoryRepository(repository)
                 .maxMessages(10)
                 .build();
-    }*/
+    }
     @Bean
     public ChatClient chatClient(ChatClient.Builder builder, ChatMemory chatMemory) {
         return builder
